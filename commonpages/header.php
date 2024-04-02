51fc37ba5013d52eb82cb18a54a6f123c4e5dae3
paurnima <paurnimajangam@gmail.com>
2024-04-02 02:18:01 -0700

    Initial commit

---
+++ commonpages/header.php
@@ -0,0 +1,24 @@
+
+
+<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+					</div>
+					<div class="col-xs-10 text-right menu-1">
+						<ul>
+							<li class="active"><a href="index1.php">Home</a></li>
+							
+							<li><a href="about.php">about</a></li>
+							
+							<li><a href="login.php">Login</a></li>
+							<li><a href="registration.php">Registration</a></li>
+							
+							<li><a href="feedback.php">Feedback</a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		
